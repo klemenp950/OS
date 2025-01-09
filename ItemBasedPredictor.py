263a00abc146e68a2fd845a4148e967ebdcff5a9
klemenp950 <klemen.parkelj@gmail.com>
2025-01-09 22:25:59 +0100

    ItemBasedPredictor done

--- ItemBasedPredictor.py
+++ ItemBasedPredictor.py
@@ -1,62 +1,86 @@
 from MovieData import MovieData
+import pandas as pd
 
 class ItemBasedPredictor:
     def __init__(self, min_values=0, threshold=0):
-        self.__uim = None
+        self.__uid = None
         self.__min_values = min_values
         self.__threshold = threshold
-        self.__list = None
+        self.__all_movies_list = list()
+        self.__relevant_movies = list()
     
     def fit(self, uim):
-        self.__uim = uim
-        self.__average_grades = [0] * uim.get_number_of_users()
-        md = MovieData('data/movies.dat')
-        self.__matrix = [[0] * md.nmovies() for _ in range(uim.get_number_of_users())]
-        for (uid, mid, rating, _) in uim.data:
-            self.__matrix[uid - 1][mid - 1] = rating
+        self.__uid = uim
+        self.__uuid = sorted(list(self.__uid.data['userID'].unique()))
+        self.__umid = sorted(list(self.__uid.data['movieID'].unique()))
+        # print(self.__umid)
+        self.__matrix_ratings = [[None for _ in range(len(self.__umid))] for _ in range(len(self.__uuid))]
         
-        for i in range(uim.get_number_of_users()):
-            self.__average_grades[i] = sum(self.__matrix[i]) / self.count(self.__matrix[i])
-        
-        for i in range(len(self.__matrix)):
-            for j in range(len(self.__matrix[i])):
-                if self.__matrix[i][j] != 0:
-                    self.__matrix[i][j] = self.__matrix[i][j] - self.__average_grades[i]
-        
-        self.__similarity_matrix = [[0] * md.nmovies() for _ in range(md.nmovies())]
+        for (_, (uid, mid, rating, _)) in self.__uid.data.iterrows():
+            if(mid in self.__umid and uid in self.__uuid):
+                self.__matrix_ratings[self.__uuid.index(uid)][self.__umid.index(mid)] = rating
 
-        for i in range(md.nmovies()):
-            for j in range(i, md.nmovies()):
+        self.__df_matrix_ratings = pd.DataFrame(self.__matrix_ratings, columns=self.__umid, index=self.__uuid)
+        self.__df_matrix_ratings.replace(0, pd.NA, inplace=True)
+
+        self.__averages = list(self.__df_matrix_ratings.mean(axis=1, skipna=True))
+
+        for i in range(len(self.__matrix_ratings)):
+            for j in range(len(self.__matrix_ratings[i])):
+                if self.__matrix_ratings[i][j]:
+                    self.__matrix_ratings[i][j] = self.__matrix_ratings[i][j] - self.__averages[i]
+        
+        self.__similarity_matrix = [[0 for _ in range(len(self.__umid))] for _ in range(len(self.__umid))]
+
+        for i in range(len(self.__umid)):
+            for j in range(i, len(self.__umid)):
                 if i == j:
                     self.__similarity_matrix[i][j] = 1
                 else:
                     sim = self.__sim(i, j)
                     self.__similarity_matrix[i][j] = sim
                     self.__similarity_matrix[j][i] = sim
-        
 
     def __sim(self, i1, i2):
         num = 0
         den1 = 0
         den2 = 0
+        counter = 0
 
-        for user_ratings in self.__matrix:
-            if user_ratings[i1] != 0 and user_ratings[i2] != 0:
+        for user_ratings in self.__matrix_ratings:
+            if user_ratings[i1] and user_ratings[i2]:
                 num += user_ratings[i1] * user_ratings[i2]
                 den1 += user_ratings[i1] ** 2
                 den2 += user_ratings[i2] ** 2
+                counter += 1
         
         
         den = (den1 ** 0.5) * (den2 ** 0.5)
-        return num / den if den != 0 else 0
+        return num / den if den != 0 and counter >= self.__min_values and num / den > 0 else 0
     
     def similarity(self, i1, i2):
-        return self.__similarity_matrix[i1][i2]
+        return self.__similarity_matrix[self.__umid.index(i1)][self.__umid.index(i2)]
+    
+    def predict(self, userID, rec_seen=True):
+        if self.__uid is None:
+            raise ValueError("Error: fit() method was not called yet.")
+        
+        predictions = {}
 
-    @staticmethod
-    def count(self, array):
-        counter = 0
-        for i in array:
-            if i != 0:
-                counter += 1
-        return counter
+        for j in range(len(self.__matrix_ratings[0])):
+            if self.__matrix_ratings[self.__uuid.index(userID)][j] is None:
+                num = 0
+                den = 0
+                for i in range(len(self.__matrix_ratings[0])):
+                    if self.__matrix_ratings[self.__uuid.index(userID)][i]:
+                        num += self.__similarity_matrix[j][i] * self.__matrix_ratings[self.__uuid.index(userID)][i]
+                        den += self.__similarity_matrix[j][i]
+                if den != 0:
+                    predictions[self.__umid[j]] = (num / den) + self.__averages[self.__uuid.index(userID)]
+        
+        if rec_seen:
+            for el in self.__umid:
+                if self.__matrix_ratings[self.__uuid.index(userID)][self.__umid.index(el)]:
+                    predictions[el] = self.__matrix_ratings[self.__uuid.index(userID)][self.__umid.index(el)]
+        else:
+            return predictions
--- test.py
+++ test.py
@@ -1,7 +1,18 @@
 from UserItemData import UserItemData
-import pandas as pd
+from MovieData import MovieData
+from RandomPredictor import RandomPredictor
+from Recommender import Recommender
+from AveragePredictor import AveragePredictor
+from ViewsPredictor import ViewsPredictor
+from ItemBasedPredictor import ItemBasedPredictor
 
-uid = UserItemData('data/user_ratedmovies.dat')
-# pd.set_option('display.max_rows', None)
-for (_, (uid, mid, _, _)) in uid.data.iterrows():
-    print(uid, mid)
+md = MovieData('data/movies.dat')
+uim = UserItemData('data/user_ratedmovies.dat', min_ratings=1000)
+print(uim.nrating())
+rp = ItemBasedPredictor()
+rec = Recommender(rp)
+rec.fit(uim)
+print("Predictions for 78: ")
+rec_items = rec.recommend(78, n=15, rec_seen=False)
+for idmovie, val in rec_items.items():
+    print("Film: {}, ocena: {}".format(md.get_title(idmovie), val))
